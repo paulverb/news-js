209240ce727b245c9278b3978dd71937139a55e5
Pavel Verbitsky <plov235@gmail.com>
2022-12-21 11:25:22 +0100

    feat: migrate sources.js to typescript

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import {Source} from '../../../types/types'
+
+class Sources {
+    draw(data: Source[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
+
+            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')!.append(fragment);
+    }
+}
+
+export default Sources;
