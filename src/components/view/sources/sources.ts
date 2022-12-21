0ccfcbd9337dd491beb361019a5b76ecdf8d2fe0
Pavel Verbitsky <plov235@gmail.com>
2022-12-21 11:40:49 +0100

    refactor: change Element to HTMLElement in sources.ts

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -7,7 +7,7 @@ class Sources {
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
         data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
             sourceClone.querySelector('.source__item-name')!.textContent = item.name;
             sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
